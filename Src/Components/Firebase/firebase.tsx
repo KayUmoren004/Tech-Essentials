a7ddb657871d638e94319e0d96e8acc02163d8ae
Godson Umoren <umorenkaykay@gmail.com>
2021-01-18 16:26:30 -0500

    Update firebase.tsx

--- Src/Components/Firebase/firebase.tsx
+++ Src/Components/Firebase/firebase.tsx
@@ -1,8 +1,3 @@
 export default {
-    apiKey: "AIzaSyCDIcY0xbcaB2PVkCO7jaT1AAi2JnW-CRg",
-  authDomain: "tech-essentials.firebaseapp.com",
-  projectId: "tech-essentials",
-  storageBucket: "tech-essentials.appspot.com",
-  messagingSenderId: "133112997665",
-  appId: "1:133112997665:web:317bddb7ad159172270060"
+   
 }
